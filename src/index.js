e62db862eae7ec274ab6697490d601e52ec8b892
murphy <1243917465@qq.com>
2022-02-24 14:29:33 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
+import './x.css'
 console.log("hi");
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
     // 文字加hash是为了取消缓存，以便更新
     filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "My App",
+      template: "src/assets/test.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        // css-loader 把 css 内容读到 js 里
+        // style-loader 把 css-loader 读到的内容变成 style 标签放到 head 里
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
